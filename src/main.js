0ed7d44c52c2bff64298cff14ea29646c247c135
Pim Dubois <pim.dubois@capgemini.com>
2023-12-05 17:10:43 +0100

    fixed folder name

--- src/main.js
+++ src/main.js
@@ -4,9 +4,9 @@ import 'bootstrap/dist/js/bootstrap.bundle.min';
 import { createPinia } from 'pinia';
 import { createApp } from 'vue';
 
-import BaseButton from './components/UI/BaseButton.vue';
-import BaseCard from './components/UI/BaseCard.vue';
-import BaseSpinner from './components/UI/BaseSpinner.vue';
+import BaseButton from './components/ui/BaseButton.vue';
+import BaseCard from './components/ui/BaseCard.vue';
+import BaseSpinner from './components/ui/BaseSpinner.vue';
 
 import App from './App.vue';
 import router from './router';
